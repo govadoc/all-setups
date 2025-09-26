10f7f5d958872d79c2a6347ff220be844fcc890d
rappa-rappa-git-hub <112715672+rappa-rappa-git-hub@users.noreply.github.com>
2025-09-26 22:48:28 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -10,8 +10,6 @@ sudo mv kops /usr/local/bin/kops
 chmod +x kubectl
 mv kubectl /usr/local/bin/kubectl
 
-aws s3api create-bucket --bucket mustafa.project.flm.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket mustafa.project.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://mustafa.project.flm.k8s.local
-kops create cluster --name mustafa.k8s.local --zones us-east-1a,us-east-1b --master-count=1 --master-size t2.large --master-volume-size=30 --node-count=3 --node-size t2.medium --node-volume-size=20
-kops update cluster --name mustafa.k8s.local --yes --admin
+export KOPS_STATE_STORE=s3://gova.flm.k8s
+kops create cluster --name gova.k8s.local --zones us-east-1a,us-east-1b --control-plane-count=1 --control-plane-size t2.large --control-plane-volume-size=30 --node-count=3 --node-size t2.medium --node-volume-size=20
+kops update cluster --name gova.k8s.local --yes --admin
